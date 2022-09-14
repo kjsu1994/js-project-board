a2def06066cd0c1565f61ad1e6625e3307b05b0d
js kim <kjsu1994@gmail.com>
2022-09-14 14:59:42 +0900

    first commit

---
+++ src/main/java/com/example/JSprojectboard/JsProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.JSprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsProjectBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JSprojectboard/JsProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JSprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
